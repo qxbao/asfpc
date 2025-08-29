c67d43522f313207b507e2c9ebbb562edd663d0c
Duong Quan Bao <dquanbao@gmail.com>
2025-08-29 14:47:06 +0700

    feat: add initial models modules

---
+++ packages/database/models/__init__.py
@@ -0,0 +1,15 @@
+from .base import Base
+from .account import Account
+from .proxy import Proxy
+from .group import Group
+from .post import Post
+from .comment import Comment
+
+__all__ = [
+    "Base",
+    "Account", 
+    "Proxy", 
+    "Group", 
+    "Post", 
+    "Comment"
+]
